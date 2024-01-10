42f5a578e540c36eb42ee9236a7ebcdda6520b7e
MarioMatsas <114489498+MarioMatsas@users.noreply.github.com>
2024-01-10 22:30:02 +0200

    Add files via upload

---
+++ DOMES Ergasia-3/TaxEvasionInterface.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public interface TaxEvasionInterface {
+    void insert(LargeDepositor item);
+    void load(String filename);
+    void updateSavings(int AFM, double savings);
+    LargeDepositor searchByAFM(int AFM);
+    List searchByLastName(String last_name);
+    void remove(int AFM);
+    double getMeanSavings();
+    void printΤopLargeDepositors(int k);
+    void printByAFM();
+}
